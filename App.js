f9d11356ce1050e1ea37b3ee91e95d1050b73ca8
hyein0112 <bhi12134@gmail.com>
2023-04-04 00:19:46 +0900

    Style

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+console.log(SCREEN_WIDTH);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello! I made a RN App!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>GwangJu</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +40,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "pink",
+  },
+  city: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    color: "black",
+    fontSize: 38,
+    fontWeight: 600,
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 158,
+    marginTop: 50,
+  },
+  description: {
+    fontSize: 48,
+    marginTop: -30,
   },
 });
